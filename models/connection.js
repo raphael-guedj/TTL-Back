4ab6aa5d26d38d71ce59d4bed53ed1bc10186b4b
raphael-guedj <raphael.guedj1@gmail.com>
2020-12-21 16:27:10 +0100

    init ttl webapp with react

--- models/connection.js
+++ models/connection.js
@@ -7,7 +7,7 @@ var options = {
 };
 
 mongoose.connect(
-  "mongodb+srv://admin:ZJsZgfHsPOOGFqDy@cluster0.gyhgs.mongodb.net/ttl-bdd?retryWrites=true&w=majority",
+  "mongodb+srv://admin:admindatabase87@cluster0.uqtso.mongodb.net/forky_webapp?retryWrites=true&w=majority",
   options,
   function (err) {
     console.log(err);
---
+++ reactapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Forky Test</h1>
+      <p>Ceci est un test pour Forky sur React </p>
+    </div>
+  );
+}
+
+export default App;
